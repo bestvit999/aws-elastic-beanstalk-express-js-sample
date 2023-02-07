3f5b93f56ae5e576f27ec17239ac1cf5e146c298
bestvit999 <bestvit999@gmail.com>
2023-02-07 18:22:20 +0800

    update message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! my first app on aws'));
+app.get('/', (req, res) => res.send('Hello World! test on my CodePipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
